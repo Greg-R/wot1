a2b88b456716ea47d7e59b45d25d47173cb1f13b
Greg <soapy-smith@comcast.net>
2017-03-19 17:38:08 -0400

    Fixed sensors.js

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var resources = require('./../resources.model');
+var resources = require('./../resources/model');
 
 router.route('/').get(function (req, res, next) {
 	res.send(resources.pi.sensors);
