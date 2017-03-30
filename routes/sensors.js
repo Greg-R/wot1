8d8eeeaab09d15b5ffe04db94c18369f149e3980
Greg-R <soapy-smith@comcast.net>
2017-03-29 21:53:14 -0400

    Updated sensors.js

--- routes/sensors.js
+++ routes/sensors.js
@@ -1,3 +1,31 @@
+// Final version
+var express = require('express'),
+  router = express.Router(),
+  resources = require('./../resources/model');
+
+router.route('/').get(function (req, res, next) {
+  req.result = resources.pi.sensors; //#A
+  next(); //#B
+});
+
+router.route('/pir').get(function (req, res, next) {
+  req.result = resources.pi.sensors.pir;
+  next();
+});
+
+router.route('/temperature').get(function (req, res, next) {
+  req.result = resources.pi.sensors.temperature;
+  next();
+});
+
+router.route('/humidity').get(function (req, res, next) {
+  req.result = resources.pi.sensors.humidity;
+  next();
+});
+
+module.exports = router;
+
+/*
 var express = require('express');
 var router = express.Router();
 var resources = require('./../resources/model');
@@ -23,3 +51,4 @@ router.route('/humidity').get(function (req, res, next) {
 });
 
 module.exports = router;
+*/
