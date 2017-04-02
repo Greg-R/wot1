fb6e2b7739ec1d973efd99704cc5c6730aa841aa
Greg-R <soapy-smith@comcast.net>
2017-04-02 14:48:50 -0400

    Updated

--- servers/websockets.js
+++ servers/websockets.js
@@ -2,45 +2,51 @@ var WebSocketServer = require('ws').Server,
     resources = require('./../resources/model');
 
 exports.listen = function (server) {
-    var wss = new WebSocketServer({
-        server: server
-    });
-    console.info('WebSocket server started...');
+        var wss = new WebSocketServer({
+            server: server
+        });
+        console.info('WebSocket server started...');
 
-    setTimeout(
-        wss.server.send('{"temperature": 100}', () => {
-            console.log("Fired data from server WebSocket.");         
-        }), 1000);
+        // Broadcast to all.
+        wss.broadcast = function broadcast(data) {
+            wss.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    client.send(new Date().toTimeString());
+                }
+            });
+        };
 
-    wss.onopen = (open) => {
-        console.info('The onopen event was fired, the websocket opened.');
-    };
+        setTimeout(wss.broadcast, 1000);
 
-    wss.on('connection', function (ws) {
-        var url = ws.upgradeReq.url;
-        console.info(url);
-        setTimeout(
-            ws.send('{"temperature": 100}'), 1000);
-        try {
-            //  Object.observe is depracated.  Using Proxy instead.
-            /*            Object.observe(selectResource(url), function (changes){
-                            ws.send(JSON.stringify(changes[0].object), function () {
-                            });                   
-                            });*/
-        } catch (e) {
-            console.log('Unable to observe %s resource!', url);
+        wss.onopen = (open) => {
+            console.info('The onopen event was fired, the websocket opened.');
+        };
+
+        wss.on('connection', function (ws) {
+                var url = ws.upgradeReq.url;
+                console.info(url);
+                setTimeout(
+                    ws.send(new Date().toTimeString(), 1000);
+                    try {
+                        //  Object.observe is depracated.  Using Proxy instead.
+                        /*            Object.observe(selectResource(url), function (changes){
+                                        ws.send(JSON.stringify(changes[0].object), function () {
+                                        });                   
+                                        });*/
+                    } catch (e) {
+                        console.log('Unable to observe %s resource!', url);
+                    }
+                    // onopen creates an event listener which fires when the websocket opens a connection.
+
+                });
+        };
+
+        function selectResource(url) {
+            var parts = url.split('/');
+            parts.shift();
+            var result = resources;
+            for (var i = 0; i < parts.length; i++) {
+                result = result[parts[i]];
+            }
+            return result;
         }
-        // onopen creates an event listener which fires when the websocket opens a connection.
-
-    });
-};
-
-function selectResource(url) {
-    var parts = url.split('/');
-    parts.shift();
-    var result = resources;
-    for (var i = 0; i < parts.length; i++) {
-        result = result[parts[i]];
-    }
-    return result;
-}
