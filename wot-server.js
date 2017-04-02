c70242694dd6b2ed29abd61f1463cae8289ac61c
Greg-R <soapy-smith@comcast.net>
2017-04-02 18:18:57 -0400

    Updated

--- wot-server.js
+++ wot-server.js
@@ -12,6 +12,8 @@ dhtPlugin.start({'simulate': false, 'frequency': 10000});
 
 var server = httpServer.listen(resources.pi.port, function() {
 	console.info('Your WoT Pi is up and running on port %s', resources.pi.port);
-    wsServer.listen(server);
-    console.info('Your WoT Pi is up and running on port %s', resources.pi.port);
 });
+                               
+    wsServer.listen(server);
+ 
+
