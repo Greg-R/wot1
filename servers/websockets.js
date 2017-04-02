8545df9c5cd12a9047fff32aaa7aca6d23ae1a0b
Greg-R <soapy-smith@comcast.net>
2017-04-02 12:27:45 -0400

    Updated

--- servers/websockets.js
+++ servers/websockets.js
@@ -14,6 +14,7 @@ exports.listen = function (server) {
     wss.on('connection', function (ws) {
         var url = ws.upgradeReq.url;
         console.info(url);
+        ws.send('Sending test string');
         try {
             //  Object.observe is depracated.  Using Proxy instead.
             /*            Object.observe(selectResource(url), function (changes){
