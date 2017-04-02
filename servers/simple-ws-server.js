6c0620d6302a774f3ecf441432995b9d50caff7f
Greg-R <soapy-smith@comcast.net>
2017-04-02 17:33:28 -0400

    Updated

---
+++ servers/simple-ws-server.js
@@ -0,0 +1,15 @@
+var WebSocket = require('ws');
+var ws = new WebSocket('ws://192.168.1.10/');
+ws.on('open', function() {
+    console.log('connected');
+    ws.send(Date.now().toString(), {mask: true});
+});
+ws.on('close', function() {
+    console.log('disconnected');
+});
+ws.on('message', function(data, flags) {
+    console.log('Roundtrip time: ' + (Date.now() - parseInt(data)) + 'ms', flags);
+    setTimeout(function() {
+        ws.send(Date.now().toString(), {mask: true});
+    }, 500);
+});
